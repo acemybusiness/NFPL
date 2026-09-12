197d88fcf1eff685d2892089c82914c3e8c05bc6
acemybusiness <acemybusiness.app@gmail.com>
2026-09-12 17:27:41 -0400

    Wire NFPL phone PWA icon and preserve v3.7.6 app

--- phone/sw.js
+++ phone/sw.js
@@ -1,1 +1,5 @@
-const CACHE='nfpl-phone-control-v1-3-1-car-restore';const ASSETS=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+const CACHE='nfpl-phone-v3.7.6-icon-v3763';
+const ASSETS=['./','./index.html','./app-v376.html?v=3763','./manifest.webmanifest?v=3763','./icons/nfpl-icon-v3763.svg?v=3763'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
