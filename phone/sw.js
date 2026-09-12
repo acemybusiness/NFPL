22d1f5900709aec26f4f2bb6a88e7419a9206576
acemybusiness <acemybusiness.app@gmail.com>
2026-09-12 17:49:49 -0400

    Refresh NFPL phone PWA cache for embedded icon fix

--- phone/sw.js
+++ phone/sw.js
@@ -1,31 +1,5 @@
-const CACHE='nfpl-phone-v3.7.6-icon-v3767';
-const ASSETS=[
-  './',
-  './index.html',
-  './app-v376.html?v=3767',
-  './manifest.webmanifest?v=3767',
-  './icons/nfpl-icon-192-v3767.png?v=3767'
-];
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request))
-  );
-});
+const CACHE='nfpl-phone-v3.7.6-icon-v3768';
+const ASSETS=['./','./index.html','./app-v376.html?v=3768','./manifest.webmanifest?v=3768','./icons/nfpl-icon-192-v3767.png?v=3768'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
